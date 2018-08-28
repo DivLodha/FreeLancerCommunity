dacef311c94d3959a69242b15017e7a3c49ecc25
divlodha <lodha.div17@gmail.com>
2018-08-28 14:48:47 +0530

    Prepared for deploy1

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-Parser");
-const passport = require("passport");
-const path = require("path");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-Parser');
+const passport = require('passport');
+const path = require('path');
 
 const app = express();
 
@@ -12,12 +12,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //routes config
-const users = require("./routes/api/users");
-const profile = require("./routes/api/profile");
-const posts = require("./routes/api/posts");
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 //DB Config
-const db = require("./config/keys").mongoURI;
+const db = require('./config/keys').mongoURI;
 
 //connect to DB
 mongoose
@@ -25,26 +25,26 @@ mongoose
     db,
     { useNewUrlParser: true }
   )
-  .then(() => console.log("Database connected"))
+  .then(() => console.log('Database connected'))
   .catch(err => console.log(err));
 
 //passport middleware
 app.use(passport.initialize());
 
 //passport Config
-require("./config/passport")(passport);
+require('./config/passport')(passport);
 
 //use routes
-app.use("/api/users", users);
-app.use("/api/profile", profile);
-app.use("/api/posts", posts);
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 //Server static assets if in production
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   //Set static folder
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
